bb3a299a095f14f9cb65f66ba5161bf0c440a329
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-01-29 13:04:31 +0000

    Add files via upload

---
+++ Why Params/Why Params/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Why_Params
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum1 = Sum(1, 2, 3);
+            int sum2 = Sum(4, 5, 6, 7, 8);
+            int sum3 = Sum();
+
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+            Console.WriteLine(sum3);
+
+            double average1 = Average(1, 2, 3);
+            double average2 = Average(4, 5, 6, 7, 8);
+            double Average3 = Average();
+
+            Console.WriteLine(average1);
+            Console.WriteLine(average2);
+            Console.WriteLine(Average3);
+        }
+
+        // Declare a method that uses the "params" keyword
+
+        public static int Sum(params int[] numbers)
+        {
+            int total = 0;
+
+            foreach (int number in numbers)
+            {
+                total += number;
+            }
+
+            return total;
+
+        }
+
+        // Declare a method that uses the "params" keyword and calculates the average
+
+        public static double Average(params int[] numbers)
+        {
+            int total = 0;
+            int count = 0;
+
+            foreach (int number in numbers)
+            {
+                total += number;
+                count++;
+            }
+
+            return (double)total / count;
+
+        }
+    }
+}
---
+++ Why foreach/Why foreach/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Why_foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            // Use foreach to perform an operation on each element in an array without manipulating the index
+            foreach(int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            // Use for when you need more control over the iteration process
+            for(int i = 0; i < numbers.Length; i+= 2)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            // Use while when you do not know how many times you will need to loop
+            string input;
+            do
+            {
+                Console.WriteLine("Please enter a valid number:");
+                input = Console.ReadLine();
+            }
+            while (!int.TryParse(input, out _));
+        }
+    }
+}
