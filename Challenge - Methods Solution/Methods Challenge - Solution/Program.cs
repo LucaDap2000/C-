1eef9aa02ce60c5279573531efcec30bdd7df692
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-01-29 11:32:17 +0000

    Add files via upload

---
+++ Challenge - Methods Solution/Methods Challenge - Solution/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Methods_Challenge___Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string friend1 = "Frank";
+            string friend2 = "Michael";
+            string friend3 = "Wlad";
+
+            GreetFriend(friend1);
+            GreetFriend(friend2);
+            GreetFriend(friend3);
+            Console.Read();
+        }
+
+        public static void GreetFriend(string friendName)
+        {
+            Console.WriteLine("Hi " + friendName + " , my friend!");
+        }
+
+    }
+
+}
