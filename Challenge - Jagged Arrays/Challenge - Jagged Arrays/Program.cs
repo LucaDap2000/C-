d3831778c4bf38446f2fc8eca3684a16842ff70f
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-01-29 11:31:33 +0000

    Add files via upload

---
+++ Challenge - Jagged Arrays/Challenge - Jagged Arrays/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Challenge___Jagged_Arrays
+{
+    internal class Program
+    {
+
+        // Create a jagged array which contains 3 "family arrays" in which two family members should be stored
+        // Introduce family members from different families to eachother via console
+
+        static void Main(string[] args)
+        {
+            string[][] jaggedArrays = new string[3][];
+            {
+                jaggedArrays[0] = new string[2];
+                jaggedArrays[1] = new string[2];
+                jaggedArrays[2] = new string[2];
+
+                jaggedArrays[0] = new string[] {"Jane","Giuliano"};
+                jaggedArrays[1] = new string[] {"Giovanni","Alessandro"};
+                jaggedArrays[2] = new string[] {"Alessio","Enzo"};
+
+                foreach (string i in jaggedArrays[0])
+                {
+                    Console.WriteLine("Hello! {0}, {1}, {2} and {3} My name is {4}, nice to meet you :)", jaggedArrays[1][0], jaggedArrays[1][1], jaggedArrays[2][0], jaggedArrays[2][1], i);
+                }
+
+                foreach (string i in jaggedArrays[1])
+                {
+                    Console.WriteLine("Hello! {0}, {1}, {2} and {3} My name is {4}, nice to meet you :)", jaggedArrays[0][0], jaggedArrays[0][1], jaggedArrays[2][0], jaggedArrays[2][1], i);
+                }
+
+                foreach (string i in jaggedArrays[2])
+                {
+                    Console.WriteLine("Hello! {0}, {1}, {2} and {3} My name is {4}, nice to meet you :)", jaggedArrays[0][0], jaggedArrays[0][1], jaggedArrays[1][0], jaggedArrays[1][1], i);
+                }
+
+                Console.Read();
+
+            }
+        }
+    }
+}
---
+++ Challenge - Methods/Challenge - Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Challenge___Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string friend1 = "Peter";
+            string friend2 = "Simon";
+            string friend3 = "John";
+            GreetFriend(friend1, friend2, friend3);
+        }
+
+        public static void GreetFriend(string friend1, string friend2, string friend3)
+        {
+            Console.WriteLine("Hi {0}, my friend!", friend1);
+            Console.WriteLine("Hi {0}, my friend!", friend2);
+            Console.WriteLine("Hi {0}, my friend!", friend3);
+        }
+
+    }
+}
