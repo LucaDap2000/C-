a3fdabbebe9add541d6e356d9bf0332881b228ea
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-01-29 11:46:35 +0000

    Add files via upload

---
+++ Challenge Ternary Operators - Solution/Challenge Ternary Operators - Solution/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Challenge_Ternary_Operators___Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputTemperature = 0;
+            string temperatureMessage = string.Empty;
+            string inputValue = string.Empty;
+
+            // Takes input from console
+            Console.WriteLine("Enter the current temperature: ");
+            inputValue = Console.ReadLine();
+
+            // Validate the input as valid input integer value
+            bool validInteger = int.TryParse(inputValue, out inputTemperature);
+
+            if(validInteger)
+            {   // Condition ? iftrue : iffalse
+                // If it is valid then it will check for the conditions using nested ternary operator here
+                temperatureMessage = inputTemperature <= 15 ?
+                // True
+                "It is too cold here" :
+                // False
+                (inputTemperature >= 16 && inputTemperature <= 28) ?
+                // True
+                "It is ok" :
+                // False
+                inputTemperature > 28 ? 
+                // True
+                "It is hot here" : 
+                // False
+                "";
+                Console.WriteLine(temperatureMessage);
+                
+            }
+            else
+            {
+                // In case input value is not a valid temperature
+                Console.WriteLine("Not a valid temperature");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Classes and Objects/Classes and Objects/Car.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Objects
+{
+    internal class Car
+    {
+        // Member variable
+        private string _name; // private field typically used for storing data
+        private int _hp;
+        private string _colour;
+
+        // Default constructor
+        public Car()
+        {
+            _name = "Car";
+            _hp = 0;
+            _colour = "red";
+        }
+
+        // Partial specification constructor
+        public Car(string name, int hp = 0)
+        {
+            _name = name;
+            Console.WriteLine(name + " was created");
+            _hp = hp;
+            _colour = "red";
+        }
+
+        // Full specification constructor
+        public Car(string name, int hp, string colour)
+        {
+            _name = name;
+            Console.WriteLine(name + " was created");
+            _hp = hp;
+            _colour = colour;
+        }
+
+        // Member method
+        public void Drive()
+        {
+            Console.WriteLine(_name + " is driving");
+        }
+
+        // Member method
+        public void Details()
+        {
+            Console.WriteLine("The " + _colour + " car " + _name + " has " + _hp + " hp ");
+        }
+
+        // Member method
+        public void Stop()
+        {
+            Console.WriteLine(_name + " Stopped!");
+        }
+    }
+}
