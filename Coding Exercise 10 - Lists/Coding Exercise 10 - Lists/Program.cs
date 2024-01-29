1b2b56d8005469f28eebe0cf0e6d0201ebc7fbf9
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-01-29 14:08:14 +0000

    Add files via upload

---
+++ Coding Exercise 10 - Lists/Coding Exercise 10 - Lists/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Coding_Exercise_10___Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+
+            for (int i = 100; i < 171; i += 2)
+            {
+                list.Add(i);
+            }
+
+            foreach (int number in list)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+
+            Console.Read();
+
+        }
+
+        public static List<int> Solution()
+        {
+            var list = new List<int>();
+
+            for (int i = 100; i < 171; i += 2)
+            {
+                list.Add(i);
+            }
+
+            foreach (int number in list)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+
+            return list;
+
+        }
+    }
+}
