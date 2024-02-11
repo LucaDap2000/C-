44a5201ceda887c4315b756890595085e088a7e7
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-02-11 17:04:56 +0000

    Add files via upload

---
+++ Coding Exercise - Dictionary Practice/Coding Exercise - Dictionary Practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Coding_Exercise___Dictionary_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public class Speller
+        {
+            public static string Convert(int i)
+            {
+                Dictionary<int, string> dic = new Dictionary<int, string>(){
+            {0, "zero"},
+            {1, "one"},
+            {2, "two"},
+            {3, "three"},
+            {4, "four"},
+            {5, "five"}
+            };
+
+                if (dic.ContainsKey(i))
+                    return dic[i];
+                else
+                    return "nope";
+            }
+        }
+    }
+}
