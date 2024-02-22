48908c2e3c67a1380dae4e9540433bc89b5f12b6
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-02-22 10:51:11 +0000

    Add files via upload

---
+++ Inheritance/Inheritance/Post.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Post
+    {
+        private static int currentPostId;
+
+        // Properties
+        protected int ID { get; set; }
+        protected string Title { get; set; }
+        protected string SendByUserName { get; set; }
+        public bool IsPublic { get; set; }
+
+        /* Default constructor. If a derived class does not invoke a base
+         * class constructor explicitly, the default constructor is called implicitly
+        */
+
+        public Post()
+        {
+            ID = GetNextID();
+            Title = "My First Post";
+            IsPublic = true;
+            SendByUserName = "Luca D'Aprano";
+        }
+
+        // Instance constructor that has three parameters
+        public Post(string title, bool isPublic, string sendByUserName)
+        {
+            this.ID = 0;
+            this.Title = title;
+            this.SendByUserName = sendByUserName;
+            this.IsPublic = isPublic;
+        }
+
+        protected int GetNextID()
+        {
+            return ++currentPostId;
+        }
+
+        public void Update(string title, bool isPublic)
+        {
+            this.Title = title;
+            this.IsPublic = isPublic;
+        }
+
+        // Virtual method override of the ToString method that is inherited
+        // from System.Object
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - by {2}", this.ID, this.Title, this.SendByUserName);
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post1 = new Post("Thanks for the Birthday wishes", true, "Luca D'Aprano");
+            Console.WriteLine(post1.ToString());
+            ImagePost imagePost1 = new ImagePost("Check out my new shoes", "Luca D'Aprano", 
+                "https://images.com/shoes", true);
+            Console.WriteLine(imagePost1.ToString());
+            Console.ReadLine();
+        }
+    }
+}
