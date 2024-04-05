d2225851d4ddc614ce7f3912373ab2a39d57d7fa
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-04-05 15:05:57 +0100

    Add files via upload

---
+++ Polymorphic Parameters/Polymorphic Parameters/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphic_Parameters
+{
+    internal class Car
+    {
+        public int HP { get; set; }
+        public string Colour { get; set; }
+
+        public Car (int hp, string colour)
+        {
+            this.HP = hp;
+            this.Colour = colour;
+        }
+
+        // Default constructor
+        public Car()
+        {
+
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("HP:" + HP + " Colour:" + Colour);
+        }
+
+        public virtual void Repair()
+        {
+            Console.WriteLine("Car was repaired");
+        }
+    }
+}
