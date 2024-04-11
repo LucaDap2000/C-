385d464390915bf5d4bba912aea37ffddcf13fe8
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-04-11 16:15:53 +0100

    Add files via upload

---
+++ Read From a Text File/Read From a Text File/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Read_From_a_Text_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 1 - Reading text
+            string text = System.IO.File.ReadAllText(@"C:\Users\Luca D'Aprano\OneDrive\Documents\ReadFromTextFile.txt");
+
+            Console.WriteLine("Text file contains the following text: {0}", text);
+
+            //Example 2
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Luca D'Aprano\OneDrive\Documents\ReadFromTextFile.txt");
+
+            Console.WriteLine("Contents of textfile.txt = ");
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
