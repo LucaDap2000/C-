31d7f15e7ad91e23c31b70761ef5582ae00c7274
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-04-16 17:36:59 +0100

    Add files via upload

---
+++ Maths Class/Maths Class/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Maths_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ceiling: " + Math.Ceiling(15.3));
+            Console.WriteLine("Floor: " + Math.Floor(15.3));
+
+            int num1 = 13;
+            int num2 = 9;
+
+            Console.WriteLine("Lower of num1: {0} and num2: {1} is {2}", num1, num2, Math.Min(num1,num2));
+            Console.WriteLine("Higher of num1: {0} and num2: {1} is {2}", num1, num2, Math.Max(num1,num2));
+
+            Console.WriteLine("3 to the power of 5 is: {0}", Math.Pow(3,5));
+            Console.WriteLine("Pi is: {0}", Math.PI);
+
+            Console.WriteLine("The square root of 25 is: {0}", Math.Sqrt(25));
+            Console.WriteLine("The absolute value of -25 is: {0}", Math.Abs(-25));
+            Console.WriteLine("cosine of 1 is: {0}", Math.Cos(1));
+
+            Console.ReadLine();
+        }
+    }
+}
