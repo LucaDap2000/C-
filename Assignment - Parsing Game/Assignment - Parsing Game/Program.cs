f1b5abba6a95d4a0f973b5bc5264681c7dfbc433
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-04-16 14:00:15 +0100

    Add files via upload

---
+++ Assignment - Parsing Game/Assignment - Parsing Game/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.Tracing;
+using System.IO;
+namespace Assignment___Parsing_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Luca D'Aprano\OneDrive\Documents\input.txt");
+
+            foreach (string line in lines)
+            {
+                if (line.Contains("split"))
+                {
+                    Console.WriteLine(line);
+                    string[] words = line.Split(' ');
+                    File.WriteAllText(@"C:\Users\Luca D'Aprano\OneDrive\Documents\output.txt", words[4]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
