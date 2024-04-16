cb81d5d8d9891f5abbadfb13846d92d9c878e92e
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-04-16 18:01:14 +0100

    Add files via upload

---
+++ Random Class/Random Class/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Random_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your question");
+            Console.ReadLine();
+
+            Random dice = new Random();
+            int numEyes;
+
+            for (int i = 0; i < 10; i++)
+            {
+                numEyes = dice.Next(1,7);
+                Console.WriteLine(numEyes);
+            }
+            
+            Random yesNoMaybe = new Random();
+            int answerNum;
+            answerNum = yesNoMaybe.Next(1,4);
+
+            if (answerNum == 1)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (answerNum == 2)
+            {
+                Console.WriteLine("Maybe");
+            }
+            else 
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
