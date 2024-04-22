a804e27f6263b6c9d2e82a42d989fee7601fd6ce
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-04-22 15:31:54 +0100

    Add files via upload

---
+++ Coding Exercise - Lambda Expressions/Coding Exercise - Lambda Expressions/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Coding_Exercise___Lambda_Expressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static Func<float, float, float> Plus = (a, b) => a + b;
+        static Func<float, float, float> Minus = (a, b) => a - b;
+        static Func<float, float, float> Divide = (a, b) => a / b;
+        static Func<float, float, float> Multiply = (a, b) => a * b;
+
+        static public Dictionary<string, Func<float, float, float>> Operators = new Dictionary<string, Func<float, float, float>>
+        {
+                {"+", Plus},
+                {"-", Minus},
+                {"/", Divide},
+                {"*", Multiply}
+        };
+
+        public static Func<float, float, float> OperationGet(string s)
+        {
+            if (Operators.ContainsKey(s))
+            {
+                return Operators[s];
+            }
+            return null;
+        }
+    }
+}
