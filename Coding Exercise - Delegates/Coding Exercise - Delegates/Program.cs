ebe50558cf60ea39352b94553078a00d9a2d5e45
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-04-22 14:39:19 +0100

    Add files via upload

---
+++ Coding Exercise - Delegates/Coding Exercise - Delegates/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Coding_Exercise___Delegates
+{
+    internal class Program
+    {
+        public delegate float OperationDelegate(float a, float b);
+        static void Main(string[] args)
+        {
+
+        }
+        public static float Add(float a, float b)
+        {
+            return a + b;
+        }
+
+        public static float Subtract(float a, float b)
+        {
+            return a - b;
+        }
+
+        public static float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+
+        public static float Divide(float a, float b)
+        {
+            return a / b;
+        }
+
+        public static float ApplyOperation(float a, float b, OperationDelegate oper)
+        {
+            float result;
+            result = oper(a, b);
+
+            return result;
+        }
+    }
+}
