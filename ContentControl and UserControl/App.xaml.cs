8d2e7f08bc02f1af9f4fd42c0283f1e1915f6a96
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-05-13 13:26:11 +0100

    Add files via upload

---
+++ ContentControl and UserControl/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ContentControl_and_UserControl
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
