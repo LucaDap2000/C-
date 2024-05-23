b3cf9ff57d22a2ed0a8f9018feeeffa926da49af
LucaDap2000 <luca.daprano@outlook.com>
2024-05-23 10:48:48 +0100

    Linq - Uni Manager

---
+++ Linq - University Manager/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq___University_Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UniversityManager um = new UniversityManager();
+
+            um.MaleStudents();
+            um.FemaleStudents();
+            um.SortStudentsByAge();
+            um.AllStudentsFromBologna();
+            um.StudentAndUniversityNameCollection();
+
+            /*
+            int[] someInts = { 30, 12, 4, 3, 12 };
+            IEnumerable<int> sortedInts = from i in someInts orderby i select i;
+            IEnumerable<int> reversedInts = sortedInts.Reverse();
+
+            foreach (int i in reversedInts)
+            {
+                Console.WriteLine(i);
+            }
+
+            IEnumerable<int> reversedSortedInts = from i in someInts orderby i descending select i;
+
+            foreach (int i in reversedSortedInts)
+            {
+                Console.WriteLine(i);
+            }
+
+            
+            string input = Console.ReadLine();
+            int inputAsInt = Convert.ToInt32(input);
+
+            try
+            {
+                um.AllStudentsFromThatUni(inputAsInt);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wrong value");
+            }
+            */
+
+            Console.ReadKey();
+        }
+    }
+
+    class UniversityManager
+    {
+        public List<University> universities;
+        public List<Student> students;
+
+        // Constructor
+        public UniversityManager()
+        {
+            universities = new List<University>();
+            students = new List<Student>();
+
+            // Lets add some Universities
+            universities.Add(new University { Id = 1, Name = "Oxford"});
+            universities.Add(new University { Id = 2, Name = "Bologna" });
+
+            // Lets add some Students
+            students.Add(new Student { Id = 1, Name = "Carlos", Gender = "Male", Age = 18, UniversityId = 1 });
+            students.Add(new Student { Id = 2, Name = "Tony", Gender = "Male", Age = 34, UniversityId = 1 });
+            students.Add(new Student { Id = 3, Name = "Sophie", Gender = "Female", Age = 19, UniversityId = 2 });
+            students.Add(new Student { Id = 4, Name = "James", Gender = "Trans-gender", Age = 25, UniversityId = 2 });
+            students.Add(new Student { Id = 5, Name = "Rachel", Gender = "Female", Age = 22, UniversityId = 2 });
+        }
+
+        public void MaleStudents()
+        {
+            IEnumerable<Student> maleStudents = from student in students where student.Gender == "Male" select student;
+            Console.WriteLine("Male - Students: ");
+
+            foreach (Student student in maleStudents)
+            {
+                student.Print();
+            }
+        }
+
+        public void FemaleStudents()
+        {
+            IEnumerable<Student> femaleStudents = from student in students where student.Gender == "Female" select student;
+            Console.WriteLine("Female - Students: ");
+
+            foreach (Student student in femaleStudents)
+            {
+                student.Print();
+            }
+        }
+
+        public void SortStudentsByAge()
+        {
+            var sortedStudents = from student in students orderby student.Age select student;
+
+            Console.WriteLine("Students sorted by age: ");
+
+            foreach (Student student in sortedStudents)
+            {
+                student.Print();
+            }
+        }
+
+        public void AllStudentsFromBologna()
+        {
+            IEnumerable<Student> blStudents = from student in students
+                                              join university in universities on student.UniversityId equals university.Id
+                                              where university.Name == "Bologna"
+                                              select student;
+
+            Console.WriteLine("Students from Bologna: ");
+
+            foreach (Student student in blStudents)
+            {
+                student.Print();
+            }
+        }
+
+        public void AllStudentsFromThatUni(int Id)
+        {
+            IEnumerable<Student> myStudents = from student in students
+                                              join university in universities on student.UniversityId equals university.Id
+                                              where university.Id == Id
+                                              select student;
+
+            Console.WriteLine("Students from that Uni {0}: ", Id);
+
+            foreach (Student student in myStudents)
+            {
+                student.Print();
+            }
+        }
+
+        public void StudentAndUniversityNameCollection()
+        {
+            var newCollection = from student in students
+                                join university in universities on student.UniversityId equals university.Id
+                                orderby student.Name
+                                select new { StudentName = student.Name, UniversityName = university.Name };
+
+            Console.WriteLine("New Collection: ");
+
+            foreach (var col in newCollection)
+            {
+                Console.WriteLine("Student {0} from University {1}", col.StudentName, col.UniversityName);
+            }
+        }
+    }
+
+    class University
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("University {0} with Id {1}", Name, Id);
+        }
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        // Foreign Key
+        public int UniversityId { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Student {0} with Id {1}, Gender {2} and Age {3} from University with Id {4}", Name, Id, Gender, Age, UniversityId);
+        }
+    }
+}
