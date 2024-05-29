6e22d897268dfea6b532cbb9c149da7b236a81db
LucaDap2000 <luca.daprano@outlook.com>
2024-05-29 12:34:24 +0100

    Clean Code - Method Structuring and Use of Braces

---
+++ Clean Code - Method Structuring/OrderProcessor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Code___Method_Structuring
+{
+    internal class OrderProcessor
+    {
+        // This may look ok but these should be split up into three different methods
+        // One to validate, one to save and one to notify the customer
+        public void Order(Order order)
+        {
+            // Validate order
+            if (order.Quantity > 0)
+            {
+                // Save order
+                Console.WriteLine("Order saved");
+
+                // Notify customer
+                Console.WriteLine("Customer notified");
+            }
+        }
+
+        // It should be structured like the following:
+
+        public void ProcessOrder(Order order)
+        {
+            if (IsValid(order))
+            {
+                SaveOrder(order);
+                NotifyCustomer(order);
+            }
+        }
+
+        public bool IsValid(Order order)
+        {
+            // TODO: Validate order logic
+            return false;
+        }
+
+        private void SaveOrder(Order order)
+        {
+            // TODO: Save order logic
+        }
+
+        private void NotifyCustomer(Order order)
+        {
+            // TODO: Notify customer logic
+        }
+    }
+}
---
+++ Clean Code - Method Structuring/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Code___Method_Structuring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderProcessor orderProcessor = new OrderProcessor();
+
+            Order order = new Order();
+
+            // The if statement below without braces is fine when only one line of code is being executed
+
+            if(orderProcessor.IsValid(order))
+                Console.WriteLine("Order is valid");
+
+            // It is good practice to always use braces
+
+            if (orderProcessor.IsValid(order))
+            {
+                Console.WriteLine("Order is valid");
+            }
+
+            foreach(int i in new int[1, 2, 3])
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
