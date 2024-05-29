9586fb54ecc1d1617bf59766bdf9bde399a2f798
LucaDap2000 <luca.daprano@outlook.com>
2024-05-29 15:04:54 +0100

    SOLID Principle 1 - Single Responsibility Upload

---
+++ Single Responsibility Principle/OrderService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility_Principle
+{
+    // In practice you should separate each of these methods out into their own classes i.e., one class to log and another to notify the customer
+
+    internal class OrderService
+    {
+        private List<Order> orders = new List<Order>();
+        private OrderLogger orderLogger = new OrderLogger();
+        private OrderNotifier orderNotifier = new OrderNotifier();
+
+        public void AddOrder(Order order)
+        {
+            orders.Add(order);
+            orderLogger.LogOrder(order);
+            orderNotifier.NotifyCustomer(order);
+        }
+
+        /*
+        private void LogOrder(Order order)
+        {
+            // Log the order to a file
+            Console.WriteLine($"Order {order.Id} logged");
+        }
+        
+        private void NotifyCustomer(Order order)
+        {
+            // Send a notification to the customer
+            Console.WriteLine($"Customer notified for order {order.Id}");
+        }
+        */
+    }
+
+    public class OrderLogger
+    {
+        internal void LogOrder(Order order)
+        {
+            // Log the order to a file
+            Console.WriteLine($"Order {order.Id} logged");
+        }
+    }
+
+    public class OrderNotifier
+    {
+        internal void NotifyCustomer(Order order)
+        {
+            // Send a notification to the customer
+            Console.WriteLine($"Customer notified for order {order.Id}");
+        }
+    }
+}
---
+++ Single Responsibility Principle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility_Principle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order { Id = 1, ProductName = "Laptop", Quantity = 2, Price = 500};
+            OrderService orderService = new OrderService();
+            orderService.AddOrder(order);
+        }
+    }
+}
