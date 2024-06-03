6b5d35532b24f5d4011c2a17be9861132cbde6e5
LucaDap2000 <luca.daprano@outlook.com>
2024-06-03 12:31:41 +0100

    Generic Methods Without Generic Classes Upload

---
+++ GM Without GC/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GM_Without_GC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+
+            logger.Log<int>(10);
+            logger.Log<string>("Hello World!");
+
+            logger.Log(new {Name = "John", Age = "30"});
+
+            Console.ReadKey();
+        }
+    }
+}
