0a8c4c62d654642d1889569f471cf390c4fcb752
LucaDap2000 <luca.daprano@outlook.com>
2024-06-03 12:55:44 +0100

    Constraints for Generic Methods Upload

---
+++ Constraints For Generic Methods/Comparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constraints_For_Generic_Methods
+{
+    internal class Comparer
+    {
+        public static bool AreEqual<T>(T first, T second) where T : class
+        {
+            return first == second;
+        }
+    }
+}
---
+++ Constraints For Generic Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constraints_For_Generic_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var productOne = new Product();
+            var productTwo = new Product();
+
+            var result = Comparer.AreEqual(productOne, productTwo);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+
+    public class Product()
+    {
+
+    }
+}
