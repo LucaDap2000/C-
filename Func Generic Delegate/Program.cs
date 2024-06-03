828f426b39399bdba9fd5af31f733bf75fbfa437
LucaDap2000 <luca.daprano@outlook.com>
2024-06-03 15:01:39 +0100

    Func<> Generic Delegate Upload

---
+++ Func Generic Delegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Func_Generic_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, string> sum = (x, y) =>
+            {
+                return "Your result is: " + (x + y).ToString();
+            };
+
+            Console.WriteLine(sum(3,5));
+
+            Console.ReadKey();
+        }
+    }
+}
