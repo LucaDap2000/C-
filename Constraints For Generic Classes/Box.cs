c6e1c08af0668409ad9fe04389c6165c83b2268a
LucaDap2000 <luca.daprano@outlook.com>
2024-06-03 12:44:30 +0100

    Constraints For Generic Classes + Custom Interface Constraint Upload

---
+++ Constraints For Generic Classes/Box.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constraints_For_Generic_Classes
+{
+    internal class Box<T> where T : class
+    {
+
+    }
+}
---
+++ Constraints For Generic Classes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constraints_For_Generic_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Box<int> boxInt = new Box<int>();
+            // Box<Book> bookBox = new Box<Book>();
+
+            Repository<Product> repository = new Repository<Product>();
+            var product = new Product();
+            repository.Add(product);
+        }
+    }
+
+    class Book
+    {
+
+    }
+
+    class Product : IEntity
+    {
+        public int Id { get; }
+    }
+}
