73c1cc20ede4064d4b0ddc376bbfcba2692bfe76
LucaDap2000 <luca.daprano@outlook.com>
2024-06-03 11:36:48 +0100

    Multiple Generic Types in a Class Upload

---
+++ Multiple Generics in Class/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple_Generics_in_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int, string> box = new Box<int, string>(100, "One Hundred");
+            box.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
