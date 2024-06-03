9cc6b43ecf836081167967ebb8e05da8f5104d0f
LucaDap2000 <luca.daprano@outlook.com>
2024-06-03 14:38:13 +0100

    Generic Reflections Upload

---
+++ Generic Reflections/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Generic_Reflections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(ConfigurationManager<>);
+
+            string myName = "Luca";
+
+            if(myName.GetType() == typeof(string))
+            {
+                // Hey it's a string
+            }
+        }
+    }
+
+    internal class ConfigurationManager<T>
+    {
+        public T LoadedConfiguration { get; private set; }
+        public ConfigurationManager(T config)
+        {
+            LoadedConfiguration = config;
+        }
+
+        public static void SaveConfig(T configToSave)
+        {
+            // Logic
+        }
+    }
+}
