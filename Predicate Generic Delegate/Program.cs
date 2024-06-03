b91a1fa8b4eeaafb417ecdc34cac55d863221a99
LucaDap2000 <luca.daprano@outlook.com>
2024-06-03 15:09:12 +0100

    Predicate<> Generic Delegate Upload

---
+++ Predicate Generic Delegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predicate_Generic_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isEven = (x) =>
+            {
+                return x % 2 == 0;
+            };
+
+            Console.WriteLine(isEven(5));
+
+            List<int> ints = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var evenInts = ints.FindAll(isEven);
+
+            Console.ReadKey();
+        }
+    }
+}
