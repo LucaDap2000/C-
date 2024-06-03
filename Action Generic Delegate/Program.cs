7c217228a4c48b36e189204f0fd792d6aeb9a373
LucaDap2000 <luca.daprano@outlook.com>
2024-06-03 14:47:41 +0100

    Action<> Generic Delegate Upload

---
+++ Action Generic Delegate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Action_Generic_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action action = () => 
+            { 
+                Console.WriteLine("Hello World!"); 
+            };
+
+            action();
+
+            Action<int> numPrint = (x) =>
+            {
+                Console.WriteLine(x);
+            };
+
+            numPrint(10);
+
+            Action<float, float, float> sum = (x, y, z) =>
+            {
+                Console.WriteLine(x + y + z);
+            };
+
+            sum(1, 2, 3);
+
+            Console.ReadKey();
+        }
+    }
+}
